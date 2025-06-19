a87326e9efed4e201607d33c6cffc59b0fdd0095
Capric <capric8416@gmail.com>
2025-06-19 15:08:24 +0800

    fix markdown to html script: mermind and table

--- md_to_html.py
+++ md_to_html.py
@@ -1,8 +1,9 @@
 import os
+import re
 import subprocess
 import sys
 
-import markdown
+import markdown2
 
 mermaid_cli_path = "../node_modules/@mermaid-js/mermaid-cli/src/cli.js"
 
@@ -16,7 +17,7 @@ def convert(path: str):
 
     begin = None
     end = None
-    svg_images = []
+    images = []
     for no in range(len(lines)):
         line = lines[no]
         if line.startswith("```mermaid"):
@@ -29,22 +30,25 @@ def convert(path: str):
                 with open(input_path, mode="w", encoding="utf-8") as f:
                     f.writelines(lines[begin + 1 : end])
                 subprocess.run(
-                    ["node", mermaid_cli_path, "-i", input_path, "-o", output_path], stdout=subprocess.PIPE
+                    ["node", mermaid_cli_path, "-i", input_path, "-o", output_path],
+                    stdout=subprocess.PIPE,
                 )
                 with open(output_path, encoding="utf-8") as f:
-                    svg_images.append((begin, end, f.read()))
+                    images.append([begin, end, f.read()])
 
                 begin = None
                 end = None
 
     html_path = os.path.join(os.path.dirname(path), "index.html")
     with open(html_path, mode="w", encoding="utf-8") as f:
-        for (index, (begin, end, _svg)) in enumerate(svg_images):
-            lines[begin : end + 1] = f"{str(index) * 50}\n"
+        for index in range(len(images)):
+            (begin, end, _svg) = images[index]
+            lines[begin : end + 1] = [f"{index}\n"] * (end + 1 - begin)
+            images[index].append(re.compile(rf"<p>{index}(\n{index})+</p>"))
 
-        html = markdown.markdown("".join(lines))
-        for (index, (_begin, _end, svg)) in enumerate(svg_images):
-            html = html.replace(f"<p>{str(index) * 50}</p>", f"<div>{svg}</div>", 1)
+        html = markdown2.markdown("".join(lines), extras=["tables"])
+        for index, (_begin, _end, svg, pattern) in enumerate(images):
+            html = pattern.sub(repl=svg, string=html, count=1)
         f.write(html)
 
     if os.path.exists(input_path):
